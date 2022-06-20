6fa887e0631be2e3c2589dfa8874e3d7833225e2
Jacob Rosenthal <jacobrosenthal@gmail.com>
2022-06-19 18:09:21 -0700

    bluetooth

---
+++ src/bluetooth.rs
@@ -0,0 +1,157 @@
+use core::cell::RefCell;
+
+use defmt::{info, unwrap};
+use embassy::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy::blocking_mutex::ThreadModeMutex;
+use embassy::channel::mpmc::Sender;
+use embassy::util::{select, Either};
+use embassy_nrf::gpio::{self, Pin};
+use embassy_nrf::gpiote::{self, Channel as _};
+use nrf_softdevice::ble::{gatt_server, peripheral};
+use nrf_softdevice::{raw, Softdevice};
+
+#[nrf_softdevice::gatt_server]
+pub struct Server {
+    pub text: TextService,
+}
+
+#[nrf_softdevice::gatt_service(uuid = "9e7312e0-2354-11eb-9f10-fbc30a62cf38")]
+pub struct TextService {
+    #[characteristic(uuid = "9e7312e0-2354-11eb-9f10-fbc30a63cf38", read, write)]
+    pub message: heapless::String<20>,
+}
+
+// tasks in same executor guaranteed to not be running at same time as they cant interupt eachother
+pub static SERVER: ThreadModeMutex<RefCell<Option<Server>>> =
+    ThreadModeMutex::new(RefCell::new(None));
+
+#[embassy::task]
+pub async fn bluetooth_task(
+    sd: &'static Softdevice,
+    channel: Sender<'static, ThreadModeRawMutex, bool, 1>,
+) {
+    let dp = unsafe { <embassy_nrf::Peripherals as embassy::util::Steal>::steal() };
+
+    let server: Server = unwrap!(gatt_server::register(sd));
+
+    // services start uninitialized
+    unwrap!(server
+        .text
+        .message_set(heapless::String::from("                    ")));
+
+    // going to share these with multiple futures which will be created and
+    // destroyed complicating lifetimes otherwise
+    SERVER.borrow().replace(Some(server));
+
+    // button presses will be delivered on HiToLo or when you release the button
+    let button1 = gpiote::InputChannel::new(
+        // degrade just a typesystem hack to forget which pin it is so we can
+        // call it Anypin and make our function calls more generic
+        dp.GPIOTE_CH1.degrade(),
+        gpio::Input::new(dp.P1_02.degrade(), gpio::Pull::Up),
+        gpiote::InputChannelPolarity::HiToLo,
+    );
+
+    #[rustfmt::skip]
+    let adv_data = &[
+        0x02, 0x01, raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
+        0x03, 0x03, 0x09, 0x18,
+        0x0a, 0x09, b'H', b'e', b'l', b'l', b'o', b'R', b'u', b's', b't',
+    ];
+
+    #[rustfmt::skip]
+    let scan_data = &[
+        0x03, 0x03, 0x09, 0x18,
+    ];
+
+    let config = peripheral::Config::default();
+
+    'waiting: loop {
+        info!("Bluetooth is OFF");
+        info!("Press button to enable, press again to disconnect");
+
+        // wait here until button is pressed
+        button1.wait().await;
+
+        loop {
+            info!("advertising!");
+
+            let adv = peripheral::ConnectableAdvertisement::ScannableUndirected {
+                adv_data,
+                scan_data,
+            };
+
+            let conn_fut = peripheral::advertise_connectable(sd, adv, &config);
+
+            let conn = match select(conn_fut, button1.wait()).await {
+                // button returns if pressed and stops advertising
+                Either::First(conn) => unwrap!(conn),
+                Either::Second(_) => {
+                    info!("waiting!");
+                    continue 'waiting;
+                }
+            };
+
+            info!("connected!");
+
+            if let Some(server) = SERVER.borrow().borrow().as_ref() {
+                // Run the GATT server on the connection. This returns when the connection gets disconnected.
+                let gatt_future = gatt_server::run(&conn, server, |e| match e {
+                    ServerEvent::Text(e) => match e {
+                        TextServiceEvent::MessageWrite(val) => {
+                            defmt::info!("ble recv {}", val.as_bytes());
+                            channel.try_send(true).ok();
+                        }
+                    },
+                });
+
+                // turn off ble after button or when disconnect
+                match select(gatt_future, button1.wait()).await {
+                    Either::First(_) => continue 'waiting,
+                    Either::Second(_) => continue 'waiting,
+                }
+            }
+        }
+    }
+}
+
+#[embassy::task]
+pub async fn softdevice_task(sd: &'static Softdevice) {
+    sd.run().await;
+}
+
+pub fn softdevice_config() -> nrf_softdevice::Config {
+    nrf_softdevice::Config {
+        clock: Some(raw::nrf_clock_lf_cfg_t {
+            source: raw::NRF_CLOCK_LF_SRC_XTAL as u8,
+            rc_ctiv: 0,
+            rc_temp_ctiv: 0,
+            accuracy: raw::NRF_CLOCK_LF_ACCURACY_20_PPM as u8,
+        }),
+        conn_gap: Some(raw::ble_gap_conn_cfg_t {
+            conn_count: 6,
+            event_length: 24,
+        }),
+        conn_gatt: Some(raw::ble_gatt_conn_cfg_t { att_mtu: 256 }),
+        gatts_attr_tab_size: Some(raw::ble_gatts_cfg_attr_tab_size_t {
+            attr_tab_size: 32768,
+        }),
+        gap_role_count: Some(raw::ble_gap_cfg_role_count_t {
+            adv_set_count: 1,
+            periph_role_count: 3,
+            central_role_count: 3,
+            central_sec_count: 0,
+            _bitfield_1: raw::ble_gap_cfg_role_count_t::new_bitfield_1(0),
+        }),
+        gap_device_name: Some(raw::ble_gap_cfg_device_name_t {
+            p_value: b"HelloRust" as *const u8 as _,
+            current_len: 9,
+            max_len: 9,
+            write_perm: unsafe { core::mem::zeroed() },
+            _bitfield_1: raw::ble_gap_cfg_device_name_t::new_bitfield_1(
+                raw::BLE_GATTS_VLOC_STACK as u8,
+            ),
+        }),
+        ..Default::default()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+//! Print battery percentage on screen every 3 hours
+//!
+//! Feather nrf52840 express
+//! https://www.adafruit.com/product/4062
+//! https://learn.adafruit.com/introducing-the-adafruit-nrf52840-feather?view=all
+//! https://learn.adafruit.com/assets/68545/
+//!
+//! Adafruit 2.13" Monochrome eInk / ePaper Display FeatherWing
+//! https://www.adafruit.com/product/4195
+//! https://learn.adafruit.com/adafruit-2-13-eink-display-breakouts-and-featherwings?view=all
+//! As of April 27, 2020 we're selling a version with SSD1680 chipset, instead of the SSD1675 chipset
+//! Busy and Rst pin not connected
+//!
+//! P1_02 button
+//! P0_16 neopixel
+//! P1_10 led blue
+//! P1_15 led red
+//! P0_29 battery divided by 2
+//!
+//! DEFMT_LOG=trace cargo run --release
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use defmt::{info, unwrap};
+use nrf_softdevice_defmt_rtt as _; // global logger
+use panic_probe as _; // print out panic messages
+mod bluetooth;
+mod display;
+
+use bluetooth::{bluetooth_task, softdevice_config, softdevice_task};
+use display::display_task;
+use embassy::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy::channel::mpmc::Channel;
+use embassy::executor::Spawner;
+use embassy::time::{Duration, Timer};
+use embassy::util::Forever;
+use embassy_nrf::config::{Config, HfclkSource, LfclkSource};
+use embassy_nrf::{interrupt, Peripherals};
+use nrf_softdevice::Softdevice;
+use nrf_softdevice_s140::{sd_power_dcdc_mode_set, NRF_POWER_DCDC_MODES_NRF_POWER_DCDC_ENABLE};
+
+static CHANNEL: Forever<Channel<ThreadModeRawMutex, bool, 1>> = Forever::new();
+
+#[embassy::main(config = "embassy_config()")]
+async fn main(spawner: Spawner, _dp: Peripherals) {
+    // well use these logging macros instead of println to tunnel our logs via the debug chip
+    info!("Hello World!");
+
+    // some bluetooth under the covers stuff we need to start up
+    let config = softdevice_config();
+    let sd = Softdevice::enable(&config);
+
+    // save battery
+    unsafe {
+        sd_power_dcdc_mode_set(NRF_POWER_DCDC_MODES_NRF_POWER_DCDC_ENABLE as u8);
+    }
+
+    let c = CHANNEL.put(Channel::new());
+
+    unwrap!(spawner.spawn(softdevice_task(sd)));
+    unwrap!(spawner.spawn(bluetooth_task(sd, c.sender())));
+    // wait till SERVER Mutex is ready
+    Timer::after(Duration::from_secs(1)).await;
+    unwrap!(spawner.spawn(display_task(c.receiver())));
+}
+
+// 0 is Highest. Lower prio number can preempt higher prio number
+// Softdevice has reserved priorities 0, 1 and 3
+pub fn embassy_config() -> Config {
+    let mut config = Config::default();
+    config.hfclk_source = HfclkSource::ExternalXtal;
+    config.lfclk_source = LfclkSource::ExternalXtal;
+    config.time_interrupt_priority = interrupt::Priority::P2;
+    // if we see button misses lower this
+    config.gpiote_interrupt_priority = interrupt::Priority::P7;
+    config
+}
+
+// WARNING may overflow and wrap-around in long lived apps
+defmt::timestamp! {"{=usize}", {
+        use core::sync::atomic::{AtomicUsize, Ordering};
+
+        static COUNT: AtomicUsize = AtomicUsize::new(0);
+        // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+        let n = COUNT.load(Ordering::Relaxed);
+        COUNT.store(n + 1, Ordering::Relaxed);
+        n
+    }
+}
